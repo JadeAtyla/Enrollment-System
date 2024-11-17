1c6b5d5bce68eaced516d6ca1fcf37e90932421e
JadeAytla <jamadigal@gmail.com>
2024-11-17 14:03:26 +0800

    Djang React Modification with Comments

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,17 +1,25 @@
-// Sample View
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [data, setData] = useState("");
-
-  useEffect(() => {
-    axios.get("http://127.0.0.1:8000/api/example/")
-      .then(response => setData(response.data.message))
-      .catch(error => console.error(error));
-  }, []);
-
-  return <div>{data}</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- Frontend/src/App.test.js
+++ Frontend/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import DjangoReact from './DjangoReact'; // Django React ouput
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App />); // Change to DjangReact if you want to see Djang React output
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
