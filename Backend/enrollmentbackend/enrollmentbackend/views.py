22f563e7df6a7b7562ec5cd2e22df8d093ffdcfd
JadeAytla <jamadigal@gmail.com>
2024-11-17 13:52:09 +0800

    Views.py Modification

--- Backend/enrollmentbackend/enrollmentbackend/views.py
+++ Backend/enrollmentbackend/enrollmentbackend/views.py
@@ -4,4 +4,4 @@ from rest_framework.response import Response
 
 class ExampleView(APIView):
     def get(self, request):
-        return Response({"message": "Hello, React!"})
+        return Response({"message": "Hello, React! I am from Django"})
