9c820c51f6313350c88361b6561e69adf2ebf860
Nell Capili <bc.nelson.capili@cvsu.edu.ph>
2025-01-06 15:59:57 +0800

    Added sample tests in tes_urls.py

--- Backend/enrollment/api/tests/test_urls.py
+++ Backend/enrollment/api/tests/test_urls.py
@@ -1,7 +1,25 @@
 from django.test import SimpleTestCase
-
+from django.urls import reverse, resolve
+from api.views import RegisterView, CustomTokenObtainPairView, RegistrarUserView
 
 class TestUrls(SimpleTestCase):
 
     def test_register_url_is_resolved(self):
-        assert 1 == 2
+        url = reverse('register')
+        self.assertEqual(resolve(url).func.view_class, RegisterView)
+
+    def test_register_url_is_resolved(self):
+        url = reverse('login')
+        self.assertEqual(resolve(url).func.view_class, CustomTokenObtainPairView)
+
+    def test_register_url_is_resolved(self):
+        url = reverse('registrar-login')
+        self.assertEqual(resolve(url).func.view_class, RegistrarUserView)
+
+    def test_register_url_is_resolved(self):
+        url = reverse('student-login')
+        self.assertEqual(resolve(url).func.view_class, RegisterView)
+
+
+        
+    
