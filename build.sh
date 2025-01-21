949acddb038fc44108115b1b8bf945b3a7f1697f
JadeAtyla <jamadigal@gmail.com>
2025-01-21 19:16:55 +0800

    Remove debugging output and streamline directory navigation in build script

--- build.sh
+++ build.sh
@@ -2,21 +2,12 @@
 
 set -o errexit  # Exit on error
 
-# Debugging: List current directory contents
-echo "Current directory: $(pwd)"
-echo "Listing files:"
-ls -l
-
-# Navigate to backend/enrollment if it exists
-if [ -d "backend/enrollment" ]; then
-  cd backend/enrollment
-else
-  echo "Directory backend/enrollment does not exist. Exiting."
-  exit 1
-fi
+source env/Scripts/activate
 
 # Install dependencies
 pip install -r requirements.txt
+
+cd Backend/enrollment
 
 # Collect static files
 python manage.py collectstatic --no-input
