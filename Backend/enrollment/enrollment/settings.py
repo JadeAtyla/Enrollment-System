1509fba18a7c700e58fa1d1896f72e764e6fe9bd
JadeAtyla <jamadigal@gmail.com>
2025-01-22 00:47:20 +0800

    Update CORS_ALLOWED_ORIGINS to reflect new deployment URL

--- Backend/enrollment/enrollment/settings.py
+++ Backend/enrollment/enrollment/settings.py
@@ -57,7 +57,7 @@ ASGI_APPLICATION = 'enrollment.asgi.application'  # For asynchronous support
 # CORS settings to allow frontend to communicate
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://enrollment-system-0x7n.onrender.com",
+    "https://enrollment-system-gjvr.onrender.com",
 ]
 CORS_ALLOW_CREDENTIALS = True  # Enable credentials (cookies)
 
